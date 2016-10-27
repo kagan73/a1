5e6aa1393fddee20e60d0baa15dc9eec27695ae8
Kağan <kagan73@gmail.com>
2016-10-27 15:23:53 +0300

    Formlar alt klasöre taşındı

---
+++ gui/forms/umain.pas
@@ -0,0 +1,26 @@
+unit uMain;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+
+type
+  TMain = class(TForm)
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Main: TMain;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
